b437f02b084f54db110031ce55f3162678323387
dandyhuang <vivo@tx.com>
2022-08-16 20:10:21 +0800

    update

--- src/epoll.cpp
+++ src/epoll.cpp
@@ -1,9 +1,9 @@
+#include <errno.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <sys/epoll.h>
-#include <errno.h>
+#include <unistd.h>
 
 #include <cassert>
 #include <thread>
@@ -18,7 +18,7 @@
 
 // Event data includes the socket FD,
 // a pointer to a fixed sized buffer,
-// number of bytes written and 
+// number of bytes written and
 // a boolean indicating whether the worker thread should stop.
 struct event_data {
   int fd;
@@ -26,23 +26,15 @@ struct event_data {
   int written;
   bool stop;
 
-  event_data():
-  fd(-1),
-  buffer(new char[DEFAULT_RING_BUFFER_SIZE]),
-  written(0),
-  stop(false) {
+  event_data() : fd(-1), buffer(new char[DEFAULT_RING_BUFFER_SIZE]), written(0), stop(false) {}
 
-  }
-
-  ~event_data() {
-    delete[] buffer;
-  }
+  ~event_data() { delete[] buffer; }
 };
 
 // Reverse in place.
 void reverse(char* p, int length) {
   int length_before_null = length - 1;
-  int i,j;
+  int i, j;
 
   for (i = 0, j = length_before_null; i < j; i++, j--) {
     char temp = p[i];
@@ -68,27 +60,26 @@ int process_messages(processor::RingBuffer<event_data>* ring_buffer) {
       auto client_fd = e_data->fd;
       auto buffer = e_data->buffer;
       // Used to signal the server to stop.
-      //printf("\nConsumer stop value %s\n", e_data->stop ? "true" : "false");
-      if (e_data->stop)
-        goto exit_consumer;
+      // printf("\nConsumer stop value %s\n", e_data->stop ? "true" : "false");
+      if (e_data->stop) goto exit_consumer;
 
       auto buffer_length = e_data->written;
       assert(client_fd != -1);
       assert(buffer_length > 0);
 
       // Write the buffer to standard output first.
-      s = write (1, buffer, buffer_length);
+      s = write(1, buffer, buffer_length);
       if (s == -1) {
-        perror ("write");
-        abort ();
+        perror("write");
+        abort();
       }
 
       // Then reverse it and echo it back.
       reverse(buffer, buffer_length);
       s = write(client_fd, buffer, buffer_length);
       if (s == -1) {
-        perror ("echo");
-        abort ();
+        perror("echo");
+        abort();
       }
       // We are not checking to see if all the bytes have been written.
       // In case they are not written we must use our own epoll loop, express write interest
@@ -100,13 +91,51 @@ int process_messages(processor::RingBuffer<event_data>* ring_buffer) {
     prev_sequence = next_sequence;
   }
 exit_consumer:
-  printf("Finished processing all events. Server shutting down. Num events processed = %d\n", num_events_processed);
+  printf("Finished processing all events. Server shutting down. Num events processed = %d\n",
+         num_events_processed);
   return 1;
 }
 
-void event_loop(int epfd,
-                int sfd,
-                processor::RingBuffer<event_data>* ring_buffer) {
+void accept_handle(int epfd, int sfd) {
+  while (true) {
+    struct sockaddr in_addr;
+    socklen_t in_len;
+    int infd;
+    char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+
+    in_len = sizeof in_addr;
+    // No need to make these sockets non blocking since accept4() takes care of it.
+    infd = accept4(sfd, &in_addr, &in_len, SOCK_NONBLOCK | SOCK_CLOEXEC);
+    if (infd == -1) {
+      printf("accep error:%s errno:%d", strerror(errno), errno);
+      if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+        break;  // We have processed all incoming connections.
+      } else {
+        perror("accept");
+        break;
+      }
+    }
+
+    // Print host and service info.
+    int retval = getnameinfo(&in_addr, in_len, hbuf, sizeof hbuf, sbuf, sizeof sbuf,
+                         NI_NUMERICHOST | NI_NUMERICSERV);
+    if (retval == 0) {
+      printf("Accepted connection on descriptor %d (host=%s, port=%s)\n", infd, hbuf, sbuf);
+    }
+    struct epoll_event event;
+    // Register the new FD to be monitored by epoll.
+    event.data.fd = infd;
+    // Register for read events, disconnection events and enable edge triggered behavior for the FD.
+    event.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+    retval = epoll_ctl(epfd, EPOLL_CTL_ADD, infd, &event);
+    if (retval == -1) {
+      perror("epoll_ctl");
+      abort();
+    }
+  }
+}
+
+void event_loop(int epfd, int sfd, processor::RingBuffer<event_data>* ring_buffer) {
   int n, i;
   int retval;
 
@@ -115,68 +144,68 @@ void event_loop(int epfd,
   struct epoll_event* events = static_cast<epoll_event*>(calloc(MAXEVENTS, sizeof event));
 
   while (true) {
-
     n = epoll_wait(epfd, events, MAXEVENTS, -1);
 
     for (i = 0; i < n; i++) {
       current_event = events[i];
 
-      if ((current_event.events & EPOLLERR) ||
-          (current_event.events & EPOLLHUP) ||
+      if ((current_event.events & EPOLLERR) || (current_event.events & EPOLLHUP) ||
           (!(current_event.events & EPOLLIN))) {
-        // An error has occured on this fd, or the socket is not ready for reading (why were we notified then?).
+        // An error has occured on this fd, or the socket is not ready for reading (why were we
+        // notified then?).
         fprintf(stderr, "epoll error\n");
         close(current_event.data.fd);
       } else if (current_event.events & EPOLLRDHUP) {
         // Stream socket peer closed connection, or shut down writing half of connection.
         // We still to handle disconnection when read()/recv() return 0 or -1 just to be sure.
         printf("Closed connection on descriptor vis EPOLLRDHUP %d\n", current_event.data.fd);
-        // Closing the descriptor will make epoll remove it from the set of descriptors which are monitored.
+        // Closing the descriptor will make epoll remove it from the set of descriptors which are
+        // monitored.
         close(current_event.data.fd);
       } else if (sfd == current_event.data.fd) {
-        // We have a notification on the listening socket, which means one or more incoming connections.
-        while (true) {
-          struct sockaddr in_addr;
-          socklen_t in_len;
-          int infd;
-          char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+        // We have a notification on the listening socket, which means one or more incoming
+        // connections.
+        accept_handle(epfd, sfd);
+        // while (true) {
+        //   struct sockaddr in_addr;
+        //   socklen_t in_len;
+        //   int infd;
+        //   char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
 
-          in_len = sizeof in_addr;
-          // No need to make these sockets non blocking since accept4() takes care of it.
-          infd = accept4(sfd, &in_addr, &in_len, SOCK_NONBLOCK | SOCK_CLOEXEC);
-          if (infd == -1) {
-            if ((errno == EAGAIN) ||
-                (errno == EWOULDBLOCK)) {
-              break;  // We have processed all incoming connections.
-            } else {
-              perror("accept");
-              break;
-            }
-          }
+        //   in_len = sizeof in_addr;
+        //   // No need to make these sockets non blocking since accept4() takes care of it.
+        //   infd = accept4(sfd, &in_addr, &in_len, SOCK_NONBLOCK | SOCK_CLOEXEC);
+        //   if (infd == -1) {
+        //     if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+        //       break;  // We have processed all incoming connections.
+        //     } else {
+        //       perror("accept");
+        //       break;
+        //     }
+        //   }
 
-         // Print host and service info.
-          retval = getnameinfo(&in_addr, in_len,
-                               hbuf, sizeof hbuf,
-                               sbuf, sizeof sbuf,
-                               NI_NUMERICHOST | NI_NUMERICSERV);
-          if (retval == 0) {
-            printf("Accepted connection on descriptor %d (host=%s, port=%s)\n", infd, hbuf, sbuf);
-          }
+        //   // Print host and service info.
+        //   retval = getnameinfo(&in_addr, in_len, hbuf, sizeof hbuf, sbuf, sizeof sbuf,
+        //                        NI_NUMERICHOST | NI_NUMERICSERV);
+        //   if (retval == 0) {
+        //     printf("Accepted connection on descriptor %d (host=%s, port=%s)\n", infd, hbuf, sbuf);
+        //   }
 
-         // Register the new FD to be monitored by epoll.
-          event.data.fd = infd;
-          // Register for read events, disconnection events and enable edge triggered behavior for the FD.
-          event.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
-          retval = epoll_ctl(epfd, EPOLL_CTL_ADD, infd, &event);
-          if (retval == -1) {
-            perror("epoll_ctl");
-            abort();
-          }
-        }
+        //   // Register the new FD to be monitored by epoll.
+        //   event.data.fd = infd;
+        //   // Register for read events, disconnection events and enable edge triggered behavior for
+        //   // the FD.
+        //   event.events = EPOLLIN | EPOLLRDHUP | EPOLLET;
+        //   retval = epoll_ctl(epfd, EPOLL_CTL_ADD, infd, &event);
+        //   if (retval == -1) {
+        //     perror("epoll_ctl");
+        //     abort();
+        //   }
+        // }
       } else {
         // We have data on the fd waiting to be read. Read and  display it.
-        // We must read whatever data is available completely, as we are running in edge-triggered mode
-        // and won't get a notification again for the same data.
+        // We must read whatever data is available completely, as we are running in edge-triggered
+        // mode and won't get a notification again for the same data.
         bool should_close = false, done = false;
 
         while (!done) {
@@ -198,12 +227,13 @@ void event_loop(int epfd,
               perror("read");
               should_close = true;
             }
-            done = true;
+            printf("EAGAIN errno = %d, count = %d\n", errno, count) done = true;
           } else if (count == 0) {
-            // Technically we don't need to handle this here, since we wait for EPOLLRDHUP. We handle it just to be sure.
-            // End of file. The remote has closed the connection.
+            // Technically we don't need to handle this here, since we wait for EPOLLRDHUP. We
+            // handle it just to be sure. End of file. The remote has closed the connection.
             should_close = true;
             done = true;
+            printf("close fd = %d\n", entry->fd)
           } else {
             // Valid data. Process it.
             // Check if the client want's the server to exit.
@@ -214,15 +244,14 @@ void event_loop(int epfd,
 
             // Publish the ring buffer entry since all is well.
             ring_buffer->publish(next_write_index);
-            if (stop)
-              goto exit_loop;
+            if (stop) goto exit_loop;
           }
         }
 
-
         if (should_close) {
           printf("Closed connection on descriptor %d\n", current_event.data.fd);
-          // Closing the descriptor will make epoll remove it from the set of descriptors which are monitored.
+          // Closing the descriptor will make epoll remove it from the set of descriptors which are
+          // monitored.
           close(current_event.data.fd);
         }
       }
@@ -232,34 +261,32 @@ exit_loop:
   free(events);
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   int sfd, epfd, retval;
   // Our ring buffer.
   auto ring_buffer = new processor::RingBuffer<event_data>(DEFAULT_RING_BUFFER_SIZE);
 
   if (argc != 2) {
     fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-    exit (EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 
   sfd = create_and_bind(argv[1]);
-  if (sfd == -1)
-    abort ();
+  if (sfd == -1) abort();
 
   retval = make_socket_non_blocking(sfd);
-  if (retval == -1)
-    abort ();
+  if (retval == -1) abort();
 
   retval = listen(sfd, SOMAXCONN);
   if (retval == -1) {
-    perror ("listen");
-    abort ();
+    perror("listen");
+    abort();
   }
 
   epfd = epoll_create1(0);
   if (epfd == -1) {
-    perror ("epoll_create");
-    abort ();
+    perror("epoll_create");
+    abort();
   }
 
   // Register the listening socket for epoll events.
@@ -269,11 +296,10 @@ int main (int argc, char *argv[]) {
     event.events = EPOLLIN | EPOLLET;
     retval = epoll_ctl(epfd, EPOLL_CTL_ADD, sfd, &event);
     if (retval == -1) {
-      perror ("epoll_ctl");
-      abort ();
+      perror("epoll_ctl");
+      abort();
     }
   }
-
 
   // Start the worker thread.
   std::thread t{process_messages, ring_buffer};
